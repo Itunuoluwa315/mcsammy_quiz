cae12428b47da7192d26d8f67fbf8e923097de0d
Ojo Itunu Samuel <olatunjisamuel315@gmail.com>
2023-09-17 01:26:56 +0100

    Update data.js

--- app/data/data.js
+++ app/data/data.js
@@ -1,43 +1,147 @@
 export default [
     {
-        question: 'What does CSS stand for?',
-        answers: ['Computer Style Sheets', 'Creative Style Sheets', 'Cascading Style Sheets', 'Colorful Style Sheets'],
-        correct: 3
-    },
-
-    {
-        question: 'Where in an HTML document is the correct place to refer to an external style sheet?',
-        answers: ['In the <head> section', 'In the <body> section', 'At the end of the document', 'You can\'t refer to an external style sheet'],
-        correct: 1
-    },    
-    {
-        question: 'Which HTML tag is used to define an internal style sheet?',
-        answers: ['<style>', '<script>', '<headStyle>', '<css>'],
-        correct: 1
-    },    
-    {
-        question: 'Which HTML attribute is used to define inline styles?',
-        answers: ['class', 'style', 'font', 'styles'],
-        correct: 2
+      question: 'What is the process by which plants make their own food?',
+      answers: ['a) Photosynthesis', 'b) Respiration', 'c) Digestion', 'd) Fermentation'],
+      correct: 0
     },
     {
-        question: 'Which is the correct CSS syntax?',
-        answers: ['{body:color=black;}', '{body;color:black;}', 'body:color=black;', 'body {color: black;}'],
-        correct: 4
+      question: 'What is the powerhouse of the cell?',
+      answers: ['a) Nucleus', 'b) Mitochondria', 'c) Endoplasmic reticulum', 'd) Golgi apparatus'],
+      correct: 1
     },
     {
-        question: 'How do you insert a comment in a CSS file?',
-        answers: ['\' this is a comment', '/* this is a comment */', '// this is a comment', '// this is a comment //'],
-        correct: 2
+      question: 'Which gas do plants absorb from the atmosphere?',
+      answers: ['a) Oxygen', 'b) Carbon dioxide', 'c) Nitrogen', 'd) Hydrogen'],
+      correct: 1
     },
     {
-        question: 'Which property is used to change the background color?',
-        answers: ['color', 'bgcolor', 'background-color', 'bgColor'],
-        correct: 3
+      question: 'Which part of the human digestive system is responsible for absorbing most of the nutrients from food?',
+      answers: ['a) Stomach', 'b) Small intestine', 'c) Large intestine', 'd) Esophagus'],
+      correct: 1
     },
     {
-        question: 'How do you add a background color for all <h1> elements?',
-        answers: ['all.h1 {background-color:#FFFFFF;}', 'h1.setAll {background-color:#FFFFFF;}', 'h1.all {background-color:#FFFFFF;}', 'h1 {background-color:#FFFFFF;}'],
-        correct: 4
+      question: 'What is the function of the respiratory system in humans?',
+      answers: ['a) Pumping blood throughout the body', 'b) Breaking down food for energy', 'c) Exchanging oxygen and carbon dioxide', 'd) Regulating body temperature'],
+      correct: 2
+    },
+    {
+      question: 'Which structure controls the passage of substances into and out of the cell?',
+      answers: ['a) Cell membrane', 'b) Nucleus', 'c) Mitochondria', 'd) Endoplasmic reticulum'],
+      correct: 0
+    },
+    {
+      question: 'Which blood vessels carry blood away from the heart?',
+      answers: ['a) Arteries', 'b) Veins', 'c) Capillaries', 'd) Venules'],
+      correct: 0
+    },
+    {
+      question: 'Which gas is essential for all living organisms and is produced during photosynthesis?',
+      answers: ['a) Oxygen', 'b) Carbon dioxide', 'c) Nitrogen', 'd) Hydrogen'],
+      correct: 0
+    },
+    {
+      question: 'What is the function of the skeletal system?',
+      answers: ['a) Pump blood', 'b) Provide structure and support', 'c) Digest food', 'd) Produce hormones'],
+      correct: 1
+    },
+    {
+      question: 'Which organ is responsible for filtering waste products from the blood?',
+      answers: ['a) Liver', 'b) Kidney', 'c) Pancreas', 'd) Stomach'],
+      correct: 1
+    },
+    {
+      question: 'Which system is responsible for producing hormones that regulate various bodily functions?',
+      answers: ['a) Nervous system', 'b) Endocrine system', 'c) Circulatory system', 'd) Respiratory system'],
+      correct: 1
+    },
+    {
+      question: 'What is the main function of white blood cells?',
+      answers: ['a) Carry oxygen', 'b) Fight infections', 'c) Help in digestion', 'd) Produce energy'],
+      correct: 1
+    },
+    {
+      question: 'In which part of the plant does most photosynthesis occur?',
+      answers: ['a) Roots', 'b) Leaves', 'c) Stems', 'd) Flowers'],
+      correct: 1
+    },
+    {
+      question: 'What is the largest organ in the human body?',
+      answers: ['a) Brain', 'b) Liver', 'c) Heart', 'd) Lungs'],
+      correct: 1
+    },
+    {
+      question: 'Which gas do humans breathe in primarily?',
+      answers: ['a) Oxygen', 'b) Carbon dioxide', 'c) Nitrogen', 'd) Hydrogen'],
+      correct: 0
+    },
+    {
+      question: 'What is the function of the nervous system?',
+      answers: ['a) Regulate body temperature', 'b) Produce energy', 'c) Control and coordinate body activities', 'd) Produce hormones'],
+      correct: 2
+    },
+    {
+      question: 'What is the function of the circulatory system?',
+      answers: ['a) Fight infections', 'b) Break down food', 'c) Transport nutrients and oxygen to cells', 'd) Regulate body temperature'],
+      correct: 2
+    },
+    {
+      question: 'Which organ is responsible for storing bile?',
+      answers: ['a) Liver', 'b) Kidney', 'c) Pancreas', 'd) Gallbladder'],
+      correct: 3
+    },
+    {
+      question: 'Which gas is a byproduct of cellular respiration?',
+      answers: ['a) Oxygen', 'b) Carbon dioxide', 'c) Nitrogen', 'd) Hydrogen'],
+      correct: 1
+    },
+    {
+      question: 'Which process helps plants to make food using sunlight?',
+      answers: ['a) Photosynthesis', 'b) Respiration', 'c) Fermentation', 'd) Digestion'],
+      correct: 0
+    },
+    {
+      question: 'Which organ produces insulin?',
+      answers: ['a) Liver', 'b) Pancreas', 'c) Kidney', 'd) Stomach'],
+      correct: 1
+    },
+    {
+      question: 'Which part of the plant anchors it to the ground and absorbs water and nutrients?',
+      answers: ['a) Roots', 'b) Leaves', 'c) Stems', 'd) Flowers'],
+      correct: 0
+    },
+    {
+      question: 'What is the main function of red blood cells?',
+      answers: ['a) Transporting oxygen', 'b) Fighting infections', 'c) Carrying nutrients', 'd) Producing hormones'],
+      correct: 0
+    },
+    {
+      question: 'Which organ is responsible for filtering out impurities and toxins from the blood?',
+      answers: ['a) Liver', 'b) Kidney', 'c) Pancreas', 'd) Lungs'],
+      correct: 1
+    },
+    {
+      question: 'What is the function of ribosomes in a cell?',
+      answers: ['a) Produce energy', 'b) Synthesize proteins', 'c) Store genetic information', 'd) Break down waste products'],
+      correct: 1
+    },
+    {
+      question: 'Which blood type is known as the universal donor?',
+      answers: ['a) A', 'b) B', 'c) AB', 'd) O'],
+      correct: 3
+    },
+    {
+      question: 'Which gas do plants release during photosynthesis?',
+      answers: ['a) Oxygen', 'b) Carbon dioxide', 'c) Nitrogen', 'd) Hydrogen'],
+      correct: 0
+    },
+    {
+      question: 'Which system is responsible for supporting the body, protecting organs, and aiding in movement?',
+      answers: ['a) Nervous system', 'b) Muscular system', 'c) Circulatory system', 'd) Respiratory system'],
+      correct: 1
+    },
+    {
+      question: 'Which part of the brain controls balance and coordination?',
+      answers: ['a) Cerebrum', 'b) Cerebellum', 'c) Medulla oblongata', 'd) Thalamus'],
+      correct: 1
     }
 ]
